fc5761de1c4a16a1ffae01946061c29acb575e85
Mihaita Ivli <mihaitaivli@gmail.com>
2017-03-27 02:26:52 +0100

    Implement the homepage

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers'])
+angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -32,14 +32,15 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.home', {
-    url: '/home',
-    views: {
-      'mainContent': {
-        templateUrl: 'templates/home.html'
+    .state('app.home', {
+      url: '/home',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/home.html',
+          controller: 'IndexController'
+        }
       }
-    }
-  })
+    })
 
   .state('app.aboutus', {
       url: '/aboutus',
